87635d6a3058e0eba619d755881b7bb1075d6d67
MarcusSvensson-Github <82384993+MarcusSvensson-Github@users.noreply.github.com>
2022-05-03 13:49:20 +0200

    paths app.py

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/private")
+def private():
+    return render_template('private.html')
+
+@app.route("/product")
+def product():
+    return render_template('product.html')
+
+
