d77d4fd4b7b9e2ad06b1224e96389cd6709a7a29
MarcusSvensson-Github <82384993+MarcusSvensson-Github@users.noreply.github.com>
2022-05-10 11:10:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/") #välkomnssida som kan skicka dig till login eller skapa konto
 def index():
     return render_template('index.html')
+
+@app.route("/login", methods['GET', 'POST'])
+def login():   
+    if request.method == 'POST':
+        return do_login() #ej skapad funktion, bör skapa session?
+    else:
+        render_template('login.html') # vanliga loginsidan vi möts av vid login
+
+@app.route("/private")
+def private():
+    return render_template('private.html')
+
+@app.route("/product")
+def product():
+    return render_template('product.html')
