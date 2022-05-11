874496625f30f9c6ee47d1918ed71713f1bfcc66
MarcusSvensson-Github <82384993+MarcusSvensson-Github@users.noreply.github.com>
2022-05-11 11:24:04 +0200

    Update views.py
    init upplägg med:
    Blueprints i auth och views
    rendering html i auth och views men init hämtar dem

--- web/views.py
+++ web/views.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template, session, request, redirect, url_fo
 
 views = Blueprint('views', __name__) #vi lägger till blueprints så vi kan anropa den i annan fil
 
-@views.route('/') #finns här men anropas i init
+@views.route('/') #finns här men anropas i init 
+@views.route('/Home') #har vi två routes på varandra såhär så går dem till samma sida/route
 def home():
-    return 'Home'
+    return render_template('index.html') #vi renderar en html sida
