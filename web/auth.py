920b0c9e828c9e3ae9dbc8275adf4473043162ff
MarcusSvensson-Github <82384993+MarcusSvensson-Github@users.noreply.github.com>
2022-05-11 11:17:33 +0200

    fyttat lite grejer

--- web/auth.py
+++ web/auth.py
@@ -1,6 +1,6 @@
 # Här har vi allt relaterat till authentisering som login, skapa konto och routes till personliga sidor.
 
-from flask import Blueprint
+from flask import Blueprint, render_template, session, request, redirect, url_for
 
 auth = Blueprint('auth', __name__) #vi lägger till blueprints så vi kan anropa den i annan fil
 
--- web/views.py
+++ web/views.py
@@ -1,6 +1,6 @@
 # Här kommer vi ha alla våra routes till olika sidor som utgör hemsidan
 
-from flask import Blueprint
+from flask import Blueprint, render_template, session, request, redirect, url_for
 
 views = Blueprint('views', __name__) #vi lägger till blueprints så vi kan anropa den i annan fil
 
