f74c5c952750139d69135f5d65035a314a41717a
MarcusSvensson-Github <82384993+MarcusSvensson-Github@users.noreply.github.com>
2022-05-12 13:13:09 +0200

    bös

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 #vår app
 from web import create_app # hämtar create_app ifrån __init__.py inne i vårt python paket som är web mappen
 
+if __name__ == '__main__':
+    app = create_app()       #vi skapar våran app
+    app.run(debug=True)      #någon tjusig kod för uppdatera sidan när du jobbar med den 
