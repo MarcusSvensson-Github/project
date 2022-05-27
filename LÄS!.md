f46a9b7c0e8f1e97106aa190dd099cb3c1155ca5
MarcusSvensson-Github <82384993+MarcusSvensson-Github@users.noreply.github.com>
2022-05-27 16:27:14 +0200

    bös

--- LÄS!.md
+++
@@ -1,5 +0,0 @@
-jag har gjort två separata mappar nu av anledningen att våra templates och lite annan kod hamnar konflikt.
-
-
-Nu går bägge att köras om man står i vardera mapp :)
-
--- Web_v2(Marcus)/app.py
+++
@@ -1,6 +0,0 @@
-#vår app
-from web import create_app # hämtar create_app ifrån __init__.py inne i vårt python paket som är web mappen
-
-if __name__ == '__main__':
-    app = create_app()       #vi skapar våran app
-    app.run(debug=True)      #någon tjusig kod för uppdatera sidan när du jobbar med den 
